cabc9171608fef7983d6b9faf58775d7e7e18846
yuwook.kang <yuwook.kang@nhn.com>
2016-10-14 19:19:58 +0900

    회원 가입 기능 개발

--- Foo_Bar/views.py
+++ Foo_Bar/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
 
 
@@ -9,3 +12,12 @@ class HomeView(LoginRequiredMixin, TemplateView):
 class AboutView(LoginRequiredMixin, TemplateView):
     template_name = 'about.html'
 
+
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
